cfdc3c5581c664277f12a8f7de08c46ff8fbc47e
DawidWho <dawidwho@gmail.com>
2025-06-25 00:08:54 +0200

    Remove unused routers

--- front/app/app/routes/People.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  route("people", "pages/people.tsx"),
-  route("my-people", "pages/my-people.tsx"),
-] satisfies RouteConfig;
--- front/app/app/routes/Planets.ts
+++
@@ -1,6 +0,0 @@
-import { type RouteConfig, index, route } from "@react-router/dev/routes";
-
-export default [
-  route("planets", "pages/planets.tsx"),
-  route("my-planets", "pages/my-planets.tsx"),
-] satisfies RouteConfig;
